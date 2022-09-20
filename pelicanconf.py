126a22f466cf1b660646fe8e035d1d27b4cdd2ed
2lu3 <hi2lu3@gmail.com>
2022-09-20 23:00:39 +0900

    updated

---
+++ pelicanconf.py
@@ -0,0 +1,54 @@
+# -------------------------------------------------------------
+# -- 基本設定
+# -------------------------------------------------------------
+SITENAME = "2lu3"
+SITEURL = ""
+TIMEZONE = "Asia/Tokyo"
+DEFAULT_LANG = "ja"
+
+# -------------------------------------------------------------
+# -- プロフィール
+# -------------------------------------------------------------
+AUTHOR = "2lu3"
+
+SITELOGO="/assets/sitelogo.png"
+FAVICON='/assets/favicon.ico'
+
+# Blogroll
+# LINKS = (('github', 'https://github.com/2lu3'),)
+
+# Social widget
+SOCIAL = (
+    ("twitter", "https://twitter.com/hi2lu3"),
+    ("github", "https://github.com/2lu3"),
+)
+
+# -------------------------------------------------------------
+# -- ディレクトリ関連
+# -------------------------------------------------------------
+PATH = "content"
+ARTICLE_PATHS = ["articles"]
+STATIC_PATHS = ["assets", "articles"]
+
+
+# -------------------------------------------------------------
+# -- 見た目
+# -------------------------------------------------------------
+THEME = "./themes/Flex"
+
+
+
+DEFAULT_PAGINATION = 20
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# -------------------------------------------------------------
+# -- その他
+# -------------------------------------------------------------
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
